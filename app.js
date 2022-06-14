f1ebce0e13f578742c0ef4d24b77dbea0a20357c
Thiago2703 <thiagocardosorr@gmail.com>
2022-06-14 18:16:34 -0400

    CHANGA

--- app.js
+++ app.js
@@ -704,16 +704,20 @@ app.get('/p/create', async (req, res) => {
     await page.type('#password', pass, { delay: 10 });
     await page.click('button[type="submit"]', { button: 'left' })
 
-    await delay(10000);
+    await delay(5000);
+
+    if ((await page.evaluate(() => document.querySelector('.text-bold'))) !== null) {
+      throw Error('FAILED TO LOGIN IN ACCOUNT')
+    }
     const base64_1 = await page.screenshot({ encoding: "base64" });
     res.write(`<img src="data:image/png;base64,${base64_1}"></img><br>`);
 
     //await page.waitForSelector(`input[value*="${email}"]`, { visible: true, timeout: 40000 });
-    await page.waitForSelector('.flex-item-fluid.p0-5.on-tiny-mobile-text-left', { timeout: 10000 });
+    //await page.waitForSelector('.flex-item-fluid.p0-5.on-tiny-mobile-text-left', { timeout: 10000 });
     //await page.click(`.button-large`, { button: 'left' });
     //await page.waitForSelector(`input[value*="${mail}"]`);
     //await page.click(`.button-large`, { button: 'left' });
-    await delay(5000);
+    //await delay(5000);
     res.write(`{"status": "success","email":"${email}@${chosen_domain}", "pass":"${pass}"}`);
     return res.end();
     return
